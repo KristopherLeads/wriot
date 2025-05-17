994c2c266957a8eae3b5deea9c1f13fdf7aefe4f
Kristopher <kristopher@developerfirst.io>
2025-05-17 10:40:10 -0700

    Update Keyword_Extractor.go

--- Tools/Keyword_Extractor.go
+++ Tools/Keyword_Extractor.go
@@ -3,3 +3,33 @@
 // The output is N most frequent words.
 // You ned to pass two command-line args - URL and number of keywords to return.
 // Install dependency with get golang.org/x/net/html
+
+package main
+
+
+
+
+
+
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"regexp"
+	"sort"
+	"strings"
+
+	"golang.org/x/net/html"
+)
+
+
+
+
+
+// WordCount represents a word and its frequency
+type WordCount struct {
+	Word  string
+	Count int
+}
